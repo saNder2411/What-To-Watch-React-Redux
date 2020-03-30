daf44691378a786651b080be50c74b43e55f555f
Alexander Olshanskiy <sander2411ukr@gmail.com>
2020-03-30 16:48:14 +0300

    8.2 Add packages and TypeScript loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require(`path`);
 
 module.exports = {
-  entry: `./src/index.js`,
+  entry: `./src/index.tsx`,
   output: {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
@@ -27,8 +27,15 @@ module.exports = {
           {loader: `style-loader`},
           {loader: `css-loader`}
         ]
-      }
+      },
+      {
+        test: /\.(tsx|ts)?$/,
+        loader: `ts-loader`
+      },
     ],
+  },
+  resolve: {
+    extensions: [`.ts`, `.tsx`, `.js`, `json`]
   },
   devtool: `source-map`,
 };
